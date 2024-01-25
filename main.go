46ca0b909ca634dc2d4f3c5132095b9883db31f1
mikejeffers <mike.s.jeffers@gmail.com>
2024-01-25 01:34:24 -0500

    implement more internal chat state and handlers

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/redis/go-redis/v9"
@@ -40,15 +41,16 @@ func auth(conn *websocket.Conn, redis *redis.Client) *User {
 	return user
 }
 
-func reader(conn *websocket.Conn, clientChan chan string) {
+func reader(conn *websocket.Conn, clientChan chan Command) {
 	for {
 		command := Command{}
 		if conn.ReadJSON(&command) != nil {
 			fmt.Println("failed to unmarshal")
-			clientChan <- "hey"
+			clientChan <- Command{"command": "DISCONNECT"}
 			return
 		}
 		fmt.Println(command)
+		clientChan <- command
 	}
 }
 
@@ -94,6 +96,23 @@ func (s *Server) removeUser(user *User) {
 	s.broadcast("USER_LEAVE", map[string]interface{}{"user": user})
 }
 
+func (s *Server) addMessage(content string, user *User) {
+	message := Message{content, user.Username, time.Now().UTC().String()}
+	s.messages = append(s.messages, message)
+	if len(s.messages) > 10 {
+		s.messages = s.messages[1:]
+	}
+	s.broadcast("MESSAGE_ADD", map[string]interface{}{"message": message})
+}
+
+func (s *Server) writeServerStateToClient(conn *websocket.Conn) {
+	userArray := make([]*User, 0)
+	for _, v := range s.users {
+		userArray = append(userArray, v)
+	}
+	conn.WriteJSON(map[string]interface{}{"command": "ACK", "messages": s.messages, "users": userArray})
+}
+
 func main() {
 	server := Server{make(map[*websocket.Conn]bool, 0), make([]Message, 0), make(map[int64]*User)}
 	redis := redisClient()
@@ -111,16 +130,22 @@ func main() {
 		}
 		server.clients[conn] = true
 		server.addUser(user)
-		clientChan := make(chan string)
+		server.writeServerStateToClient(conn)
+		clientChan := make(chan Command)
 		go reader(conn, clientChan)
 		//
 		for {
-			select {
-			case <-clientChan:
+			command := <-clientChan
+			if command["command"] == "DISCONNECT" {
 				server.removeUser(user)
 				delete(server.clients, conn)
 				return
+			} else if command["command"] == "SEND_MESSAGE" {
+				server.addMessage(command["message"].(string), user)
+			} else if command["command"] == "REFRESH" {
+				server.writeServerStateToClient(conn)
 			}
+
 		}
 	})
 
